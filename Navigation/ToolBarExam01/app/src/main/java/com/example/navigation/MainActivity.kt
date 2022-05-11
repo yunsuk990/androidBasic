590c10a2c66f0f367d662d72125d6753ef63f260
yunsuk990 <yunsuk990@naver.com>
2022-05-11 15:41:07 +0900

    toolBar를 액티비티의 actionBar로 설정

--- Navigation/ToolBarExam01/app/src/main/java/com/example/navigation/MainActivity.kt
+++ Navigation/ToolBarExam01/app/src/main/java/com/example/navigation/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
     }
 }

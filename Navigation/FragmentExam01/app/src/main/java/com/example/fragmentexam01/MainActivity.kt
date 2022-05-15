0fe38ae0bae2a31ace1c81d3fbf7b3718c0e94a5
yunsuk990 <yunsuk990@naver.com>
2022-05-16 02:31:59 +0900

    MainActivity 구현

--- Navigation/FragmentExam01/app/src/main/java/com/example/fragmentexam01/MainActivity.kt
+++ Navigation/FragmentExam01/app/src/main/java/com/example/fragmentexam01/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.fragmentexam01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fragmentexam01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnFragment1.setOnClickListener {
+            onReplaceFragment(FragmentFirst())
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            onReplaceFragment(FragmentSecond())
+        }
+
+
+    }
+
+    fun onReplaceFragment(fragment: Fragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragmentContainerView, fragment).commit()
     }
 }

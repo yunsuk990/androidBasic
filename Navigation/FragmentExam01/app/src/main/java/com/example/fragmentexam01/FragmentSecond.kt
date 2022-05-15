433486fc44c413dbe21b98f51ccd0d9c4189f3b5
yunsuk990 <yunsuk990@naver.com>
2022-05-16 02:34:55 +0900

    FragmentSecond 이벤트 구현

--- Navigation/FragmentExam01/app/src/main/java/com/example/fragmentexam01/FragmentSecond.kt
+++ Navigation/FragmentExam01/app/src/main/java/com/example/fragmentexam01/FragmentSecond.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class FragmentSecond: Fragment() {
@@ -12,6 +13,10 @@ class FragmentSecond: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        view.findViewById<Button>(R.id.btn_go_first).setOnClickListener {
+            (activity as MainActivity).onReplaceFragment(FragmentFirst())
+        }
+        return view
     }
 }

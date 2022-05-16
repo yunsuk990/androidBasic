1f72291f1bc01e3f56a8aa64f37853f18e3045e0
yunsuk990 <yunsuk990@naver.com>
2022-05-16 14:26:10 +0900

    Update MainActivity.kt

--- Navigation/CounterExam01/app/src/main/java/com/example/counterexam1/MainActivity.kt
+++ Navigation/CounterExam01/app/src/main/java/com/example/counterexam1/MainActivity.kt
@@ -2,17 +2,50 @@ package com.example.counterexam1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.counterexam1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        const val TAG: String = "로그"
+    }
+
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewmodel by lazy {
+        ViewModelProvider(this).get(CounterViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val counterObserver = Observer<Int>{
+            binding.counterTv.text = it.toString()
+        }
+
+        viewmodel.getCounter().observe(this, counterObserver)
+
+//        code refactoring
+//        viewmodel.getCounter().observe(this, Observer{
+//            binding.counterTv.text = it.toString()
+//        })
+
+        init()
+    }
+
+    fun init() {
+        with(binding){
+            fabAdd.setOnClickListener {
+                viewmodel.increaseOrDecreaseValue(1)
+            }
+            fabRemove.setOnClickListener {
+                viewmodel.increaseOrDecreaseValue(-1)
+            }
+        }
     }
 }

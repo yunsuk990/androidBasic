967a4f0d7f6d79769d5dd3faeb86f8442eb5a6f6
yunsuk990 <yunsuk990@naver.com>
2022-05-16 15:59:28 +0900

    BackStack

--- Navigation/NavigationEx01/app/src/main/java/com/example/navigationex01/MainActivity.kt
+++ Navigation/NavigationEx01/app/src/main/java/com/example/navigationex01/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.navigationex01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.navigationex01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }

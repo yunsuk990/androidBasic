cc4b468c06ebdd8a399e69ea53eeac0805595529
yunsuk990 <yunsuk990@naver.com>
2022-05-17 20:46:18 +0900

    Create ListFragmentClass

---
+++ Navigation/FragmentExam02/app/src/main/java/com/example/fragmentexam02/ListFragment.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentexam02
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentexam02.databinding.FragmentListBinding
+
+class ListFragment: Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "ListFragment - onCreate() called")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "ListFragment - onAttach() called")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "ListFragment - onCreateView() called")
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}

9b9db6484cb3e519ea2f0e93f7ee062538fcaa5f
yunsuk990 <yunsuk990@naver.com>
2022-05-17 20:48:46 +0900

    Update

--- Navigation/FragmentExam02/app/src/main/java/com/example/fragmentexam02/ListFragment.kt
+++ Navigation/FragmentExam02/app/src/main/java/com/example/fragmentexam02/ListFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fragmentexam02.MainActivity.Companion.TAG
 import com.example.fragmentexam02.databinding.FragmentListBinding
 
 class ListFragment: Fragment() {

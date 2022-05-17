61289a2869e9b7dc06ef4996672564bb3eec1388
yunsuk990 <yunsuk990@naver.com>
2022-05-17 20:44:40 +0900

    Create HomeFragmentClass

---
+++ Navigation/FragmentExam02/app/src/main/java/com/example/fragmentexam02/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentexam02
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentexam02.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "HomeFragment - onCreate() called")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "HomeFragment - onAttach() called")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "HomeFragment - onCreateView() called")
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}

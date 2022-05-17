025a4442cb1f63259ed9fc1fbfd51214b16e52d6
yunsuk990 <yunsuk990@naver.com>
2022-05-17 20:47:41 +0900

    Create ProfileFragmentClass

---
+++ Navigation/FragmentExam02/app/src/main/java/com/example/fragmentexam02/ProfileFragment.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentexam02
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentexam02.databinding.FragmentProfileBinding
+
+class ProfileFragment: Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "ProfileFragment - onCreate() called")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "ProfileFragment - onAttach() called")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "ProfileFragment - onCreateView() called")
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}

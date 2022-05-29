598e0911f30e63d4aeaa801e50f1b356d698dd77
yunsuk990 <yunsuk990@naver.com>
2022-05-30 00:49:04 +0900

    Update MainActivity.kt

--- CoroutineTutorial/CoroutineEx02/app/src/main/java/com/example/coroutineex02/MainActivity.kt
+++ CoroutineTutorial/CoroutineEx02/app/src/main/java/com/example/coroutineex02/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.coroutineex02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coroutineex02.Fragment.MainListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, MainListFragment(), MainListFragment.TAG)
+                .commit()
+        }
+    }
+
+    override fun onBackPressed() {
+        val isPopExcists = supportFragmentManager.popBackStackImmediate()
+        if(!isPopExcists){
+            super.onBackPressed()
+        }
     }
 }
